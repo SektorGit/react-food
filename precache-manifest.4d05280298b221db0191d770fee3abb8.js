c8111344ede87d0465af2c7059fe9282de09bd61
Matt J <49405377+SektorGit@users.noreply.github.com>
2019-11-11 22:42:02 -0500

    Updates

---
+++ precache-manifest.4d05280298b221db0191d770fee3abb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46ac68c3782f2e29e34fd024b7fb7032",
+    "url": "/react-food/index.html"
+  },
+  {
+    "revision": "53a853127259ac5d6906",
+    "url": "/react-food/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "91e241524cd99c80fd90",
+    "url": "/react-food/static/js/2.45e79a48.chunk.js"
+  },
+  {
+    "revision": "53a853127259ac5d6906",
+    "url": "/react-food/static/js/main.15131c48.chunk.js"
+  },
+  {
+    "revision": "4cb20a5b8c35a115c1db",
+    "url": "/react-food/static/js/runtime-main.2d6c9406.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-food/static/media/logo.25bf045c.svg"
+  }
+]);
