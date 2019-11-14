6bfbd0d7a2b11e2011189e5682e97f7f5fd39480
Matt J <49405377+SektorGit@users.noreply.github.com>
2019-11-14 15:34:30 -0500

    SearchBar component render function added

--- src/components/Searchbar/SearchBar.js
+++ src/components/Searchbar/SearchBar.js
@@ -14,6 +14,25 @@ class SearchBar extends React.Component {
             return <li key={sortByOptionValue}>{sortByOption}</li> 
         });
     }
+
+    render() {
+        return (
+            <div className="SearchBar">
+                <div className="SearchBar-sort-options">
+                    <ul>
+                      {this.renderSortByOptions()}
+                    </ul>
+                </div>
+                <div className="SearchBar-fields">
+                    <input placeholder="Search Businesses" />
+                    <input placeholder="Where?" />
+                </div>
+                <div className="SearchBar-submit">
+                    <a>Let's Go</a>
+                </div>
+            </div>
+        )
+    }
 }
 
-
+export default SearchBar;
