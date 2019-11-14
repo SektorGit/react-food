65d844c405e8726e03f32a263cec59728e245e9e
Matt J <49405377+SektorGit@users.noreply.github.com>
2019-11-14 12:10:36 -0500

    BusinessList Component created BusinessList.js and BusinessList.css

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,3 +1,20 @@
 import React from "react";
 import "./BusinessList.css";
-import Business from "../Business/Business;"
+import Business from "../Business/Business";
+
+class BusinessList extends React.Component {
+  render() {
+      return (
+        <div className="BusinessList">
+          <Business /> 
+          <Business />
+          <Business />
+          <Business />
+          <Business />
+          <Business />
+      </div>
+      )
+  }
+}
+
+export default BusinessList;
