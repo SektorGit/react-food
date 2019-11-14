6b85891ba34ade5713f8d079698f458004a818d4
Matt J <49405377+SektorGit@users.noreply.github.com>
2019-11-14 17:07:11 -0500

    import SearcBar and import BusinessList into App.js file

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from '../../logo.svg';
 import './App.css';
+import SearchBar from '../SearchBar/Searchbar';
+import BusinessList from '../BusinessList/BusinessList';
 
 function App() {
   return (
