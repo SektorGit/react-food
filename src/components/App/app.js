d8e4a6a7d17754adcec0648807eb7aff83cac3e5
Matt J <49405377+SektorGit@users.noreply.github.com>
2019-11-15 15:30:07 -0500

    set up app.js with component imports and return compononents

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -7,20 +7,9 @@ import BusinessList from '../BusinessList/BusinessList';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList />
     </div>
   );
 }
