0300ae90f9cc8ea02f9415ccd99c0442f7ac32fb
Matt J <49405377+SektorGit@users.noreply.github.com>
2019-12-04 13:36:57 -0500

    deployment test, Heading Change

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -7,7 +7,7 @@ import BusinessList from '../BusinessList/BusinessList';
 function App() {
   return (
     <div className="App">
-      <h1>ravenous</h1>
+      <h1>React Restaurant Review Project</h1>
       <SearchBar />
       <BusinessList />
     </div>
